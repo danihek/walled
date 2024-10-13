828413538c4ec9d8e93391b55e95f2cd1462b527
danihek <danihek07@gmail.com>
2024-10-13 16:56:39 +0200

    udpate, added sorting arg

--- walled.py
+++ walled.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import datetime
 import requests
 import sys
@@ -10,15 +12,30 @@ query = sys.argv[1]
 
 pattern = r"wallhaven-(\w+)(?:\.png|\.jpg)?"
 categories=111
+sorting = "relevance"
 
-if len(sys.argv) > 1:
-    purity=sys.argv[2]
+availablesorting = [ "relevance", "random", "date_added", "views", "favourites", "toplist", "hot" ]
+
+print(len(sys.argv))
 
 if len(sys.argv) > 2:
+    purity=sys.argv[2]
+if len(sys.argv) > 3:
     categories=sys.argv[3]
+if len(sys.argv) > 4:
+    sorting=categories=sys.argv[4]
+
+match = 0
+for i in range(len(availablesorting)):
+    if sorting == availablesorting[i]:
+        match = 1
+
+if match == 0:
+    print("Bad sorting provided!")
+    exit()
 
 def wallpaper_search_api(_query):
-	url = f"https://wallhaven.cc/api/v1/search?q={_query}&categories={categories}&apikey={APIKEY}&purity={purity}"
+	url = f"https://wallhaven.cc/api/v1/search?q={_query}&categories={categories}&apikey={APIKEY}&purity={purity}&sorting={sorting}"
 	res = requests.get(url)
 	json_data = res.json()
 	dl_links = []
